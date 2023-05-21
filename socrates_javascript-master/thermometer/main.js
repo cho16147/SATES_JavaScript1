7df9ab966642c9afddde51d105be8cfdfc52373e
YongHwan CHO <64125970+cho16147@users.noreply.github.com>
2023-05-21 16:05:55 +0900

    연습용) 최초 업로드
    연습용) 최초 업로드

---
+++ socrates_javascript-master/thermometer/main.js
@@ -0,0 +1,31 @@
+/** 섭씨 온도를 입력받아 화씨 온도를 반환하는 함수를 작성하시오 */
+function celciusToFarenheit(celcius) {
+  let a = celcius * (9 / 5) + 32;
+  return a;
+}
+/** 화씨 온도를 입력받아 섭씨 온도를 반환하는 함수를 작성하시오 */
+function fahrenheitToCelcius(fahrenheit) {
+  let b = (fahrenheit - 32) * (5 / 9);
+  return b;
+}
+
+/* 이 아래는 건드리지 말것 */
+const celciusInput = document.querySelector("#celcius");
+const fahrenheitInput = document.querySelector("#fahrenheit");
+
+const c2f = document.querySelector(".cToF");
+const f2c = document.querySelector(".fToC");
+
+c2f.addEventListener("click", () => {
+  const celcius = Number(celciusInput.value);
+  if (celcius) {
+    fahrenheitInput.value = celciusToFarenheit(celcius);
+  }
+});
+
+f2c.addEventListener("click", () => {
+  const fahrenheit = Number(fahrenheitInput.value);
+  if (fahrenheit) {
+    celciusInput.value = fahrenheitToCelcius(fahrenheit);
+  }
+});
